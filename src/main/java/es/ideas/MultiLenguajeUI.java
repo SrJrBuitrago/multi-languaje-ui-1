083f95119ebd4740be3b124f19b73ffd3e282dcb
SrJrBuitrago <5588497@alu.murciaeduca.es>
2022-03-13 17:04:08 +0100

    App terminada y comentada

--- src/main/java/es/ideas/MultiLenguajeUI.java
+++ src/main/java/es/ideas/MultiLenguajeUI.java
@@ -16,7 +16,18 @@ import java.util.ResourceBundle;
 public class MultiLenguajeUI extends Application {
 
     private static Stage escenario;
-
+    // El método start crea un objeto loader de la clase FXMLLoader para cargar
+    // los recursos de la vista (primary.fxml) y los recursos de
+    // internacionalización (Resource Bundle 'cadenas').Asignamos el controlador
+    // creando un objeto de la clase PrimaryController (pc) y le asignamos el
+    // controlador mediante el método getController() de la clase FXMLLoader.
+    //Indicamos con pc.setMainWindow(this) que esta va a ser la ventana
+    // principal. Creamos una Escena y le pasamos como parámetro el parent
+    // (raiz), un ancho y un alto. Con el método setScene setteamos nuestra
+    // escena al escenario que hemos creado arriba, decimos mediante el método
+    //setResizable(false) que nuestro escenario no es expandible, le damos un
+    // título usando el método setTitle y mostramos el escenario usando el
+    // método show()
     @Override
     public void start(Stage stage) throws IOException {
         MultiLenguajeUI.escenario = stage;
@@ -32,7 +43,7 @@ public class MultiLenguajeUI extends Application {
         Scene scene = new Scene(raiz, 600, 600);
         stage.setScene(scene);
         stage.setResizable(false);
-        stage.setTitle("Multi-Lenguaje");
+        stage.setTitle("Multi-Lenguaje-UI");
         stage.show();
     }
 
--- src/main/java/es/ideas/PrimaryController.java
+++ src/main/java/es/ideas/PrimaryController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController implements Initializable {
-
+// Creamos los componentes FXML de nuestra vista
     @FXML
     private ChoiceBox<String> cbSemana;
     @FXML
@@ -27,6 +27,7 @@ public class PrimaryController implements Initializable {
     private ToggleButton tbEsp;
     @FXML
     private ToggleButton tbIta;
+
     private MultiLenguajeUI principal;
     public void setMainWindow(MultiLenguajeUI main){
         this.principal= main;
@@ -36,21 +37,28 @@ public class PrimaryController implements Initializable {
 
         //Inicialización del ComboBox con los días de la semana
         //También tiene que traducirse
+        //Tomamos las String que tenemos en nuestro Resource Bundle cadenas con
+        // las key indicadas y se lo insertamos al array de String dias_semana[]
         String dias_semana[] = {rb.getString("lunes"), rb.getString
                 ("martes"), rb.getString("miércoles"),
                 rb.getString("jueves"), rb.getString("viernes"),
                 rb.getString("sábado"), rb.getString("domingo")};
         cbSemana.setItems(FXCollections.observableArrayList(dias_semana));
+        //Creamos un ToogleGroup para agrupar nuestros ToogleButton
         ToggleGroup tGroup = new ToggleGroup();
         tGroup.getToggles().addAll(tbEsp,tbIta,tbUK,tbUsa,tbFra);
-
+        //Creo un listener del ToogleGroup para que cuando se seleccione uno
+        // de los ToogleButton del ToogleGroup mediante el condicional if
+        // compruebe el texto que hay en ese ToogleButton y dependiendo de ese
+        // texto con el método setDefault() de la clase Locale cambiará el
+        // el fichero de idiomas al asignado a ese ToogleButton
         tGroup.selectedToggleProperty().addListener((obs,oldValue,newValue)
                 ->{
             if (newValue != null ){
                 ToggleButton tButton = (ToggleButton) newValue.
                         getToggleGroup()
                         .getSelectedToggle();
-                //Se comprueba el valor del Texto del ToggleButton
+
                 switch (tButton.getText()){
                     case "Italiano":
                         Locale.setDefault(Locale.ITALIAN);
@@ -72,6 +80,8 @@ public class PrimaryController implements Initializable {
                         Locale.setDefault(new Locale("es"));
 
                 }
+                // Aquí mediante un try catch intentamos volver a cargar el
+                //escenario principal llamando al método cargadorFXML()
                 try{
                     Parent pane = cargadorFXML().load();
                     MultiLenguajeUI.getPrimaryStage().getScene().setRoot
@@ -83,7 +93,9 @@ public class PrimaryController implements Initializable {
 
         });
     }
-
+    //Asignamos los recursos cadenas y el primary.fxml al FXMLLoader.
+    // Dependiendo del recurso cadenas que tengamos seleccionado al presionar un
+    // ToogleButton se asignará un idioma u otro
     private FXMLLoader cargadorFXML(){
         FXMLLoader loader = new FXMLLoader();
         loader.setResources(ResourceBundle.getBundle(
