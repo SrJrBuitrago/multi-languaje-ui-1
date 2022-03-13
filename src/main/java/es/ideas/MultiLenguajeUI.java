d800120c76771ebcbbf98451ec963b69611d69f8
SrJrBuitrago <5588497@alu.murciaeduca.es>
2022-03-13 16:11:40 +0100

    Terminados el controlador y el main

--- src/main/java/es/ideas/MultiLenguajeUI.java
+++ src/main/java/es/ideas/MultiLenguajeUI.java
@@ -7,28 +7,37 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * JavaFX App
  */
 public class MultiLenguajeUI extends Application {
 
-    private static Scene scene;
+    private static Stage escenario;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        MultiLenguajeUI.escenario = stage;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("primary.fxml"));
+        loader.setResources(ResourceBundle.getBundle
+                ("es.ideas.i18n/cadenas",
+                        Locale.getDefault()));
+        Parent raiz = loader.load();
+        PrimaryController pc = loader.getController();
+        pc.setMainWindow(this);
+
+        Scene scene = new Scene(raiz, 600, 600);
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Multi-Lenguaje");
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MultiLenguajeUI.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public static Stage getPrimaryStage(){
+        return escenario;
     }
 
     public static void main(String[] args) {
--- src/main/java/es/ideas/PrimaryController.java
+++ src/main/java/es/ideas/PrimaryController.java
@@ -1,24 +1,96 @@
 package es.ideas;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController implements Initializable {
 
     @FXML
     private ChoiceBox<String> cbSemana;
-
+    @FXML
+    private ToggleButton tbUsa;
+    @FXML
+    private ToggleButton tbFra;
+    @FXML
+    private ToggleButton tbUK;
+    @FXML
+    private ToggleButton tbEsp;
+    @FXML
+    private ToggleButton tbIta;
+    private MultiLenguajeUI principal;
+    public void setMainWindow(MultiLenguajeUI main){
+        this.principal= main;
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         //Inicialización del ComboBox con los días de la semana
         //También tiene que traducirse
-        String dias_semana[] = {"lunes", "martes", "miércoles", "jueves",
-            "viernes", "sábado", "domingo"};
-        cbSemana.setItems(FXCollections.observableArrayList(dias_semana));        
+        String dias_semana[] = {rb.getString("lunes"), rb.getString
+                ("martes"), rb.getString("miércoles"),
+                rb.getString("jueves"), rb.getString("viernes"),
+                rb.getString("sábado"), rb.getString("domingo")};
+        cbSemana.setItems(FXCollections.observableArrayList(dias_semana));
+        ToggleGroup tGroup = new ToggleGroup();
+        tGroup.getToggles().addAll(tbEsp,tbIta,tbUK,tbUsa,tbFra);
+
+        tGroup.selectedToggleProperty().addListener((obs,oldValue,newValue)
+                ->{
+            if (newValue != null ){
+                ToggleButton tButton = (ToggleButton) newValue.
+                        getToggleGroup()
+                        .getSelectedToggle();
+                //Se comprueba el valor del Texto del ToggleButton
+                switch (tButton.getText()){
+                    case "Italiano":
+                        Locale.setDefault(Locale.ITALIAN);
+
+                        break;
+                    case "Inglés USA":
+                        Locale.setDefault(Locale.US);
+
+                        break;
+                    case "Francés":
+                        Locale.setDefault(Locale.FRENCH);
+
+                        break;
+                    case "Inglés UK":
+                        Locale.setDefault(Locale.UK);
+
+                        break;
+                    default:
+                        Locale.setDefault(new Locale("es"));
+
+                }
+                try{
+                    Parent pane = cargadorFXML().load();
+                    MultiLenguajeUI.getPrimaryStage().getScene().setRoot
+                            (pane);
+                }catch(IOException ieo){
+                }
+                MultiLenguajeUI.getPrimaryStage().show();
+            }
+
+        });
     }
+
+    private FXMLLoader cargadorFXML(){
+        FXMLLoader loader = new FXMLLoader();
+        loader.setResources(ResourceBundle.getBundle(
+                "es.ideas.i18n/cadenas",
+                Locale.getDefault()));
+        loader.setLocation(getClass().getResource("primary.fxml"));
+        return loader;
+    }
+
 }
